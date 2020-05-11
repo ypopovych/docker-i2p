872d6c8cea05a06c10dec18f0ac63491d474bf37
Yehor Popovych <yehor@crossroad.io>
2020-05-11 13:38:46 +0300

    added MEM_MAX parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,12 @@ if [[ "$PUID" != "" && "$SUID" != "$PUID" ]]; then
   usermod -u "$PUID" -g "$PGID" i2psvc
 fi
 
+if [[ -z "$MEM_MAX" ]]; then
+  MEM_MAX="128"
+fi
+
+sed -i "s/^wrapper\.java\.maxmemory=[0-9]*$/wrapper.java.maxmemory=${MEM_MAX}/g" /etc/i2p/wrapper.config
+
 # Ensure user rights
 mkdir -p /var/lib/i2p/i2p-config
 chown -R i2psvc:$PGID /var/lib/i2p
